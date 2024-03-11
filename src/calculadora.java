b9c805dde500999a8ff6c34d03821343500da87b
DanielaSMR <10828775@ieselgrao.org>
2024-03-11 13:53:03 +0100

    g

--- src/calculadora.java
+++ src/calculadora.java
@@ -1,52 +1,58 @@
 public class calculadora{
-    private int num1;
-    private int num2;
+    private double num1;
+    private double num2;
     
     public calculadora(){
 
     }
 
-    public calculadora(int num1,int num2){
+    public calculadora(double num1,double num2){
         this.num1 = num1;
         this.num2 = num2;
     }
 
-    public static int suma(int n1, int n2){
-        int resultado = n1 + n2;
+    public static double suma(double n1, double n2){
+        double resultado = n1 + n2;
+        System.out.println(resultado);
         return resultado;
     }
 
-    public static int resta(int n1, int n2){
-        int resultado = n1 - n2;
+    public static double resta(double n1, double n2){
+        double resultado = n1 - n2;
+        System.out.println(resultado);
         return resultado;
     }
 
-    public static double multiplicar(int n1, int n2) throws Exception{
-        int resultado = n1 * n2;
+    public static double multiplicar(double n1, double n2){
+        double resultado = n1 * n2;
+        System.out.println(resultado);
         return resultado;
     }
 
-    public static double dividir(int n1, int n2){
+    public static double dividir(double n1, double n2){
+        double resultado;
         if(n1 == 0 && n2 == 0){
             throw new RuntimeException("No se puede divir 0 entre 0");
+        }else{
+            resultado = n1 / n2;
         }
-
-        return n1/n2;
+        System.out.println(resultado);
+        return resultado;
     }
 
-    public int getNum1() {
+    public double getNum1() {
         return num1;
     }
 
-    public void setNum1(int num1) {
+    public void setNum1(double num1) {
         this.num1 = num1;
     }
 
-    public int getNum2() {
+    public double getNum2() {
         return num2;
     }
 
-    public void setNum2(int num2) {
+    public void setNum2(double num2) {
         this.num2 = num2;
     }
 
--- src/main.java
+++ src/main.java
@@ -5,25 +5,35 @@ public class main {
         Scanner sc = new Scanner(System.in);
         int opciones;
         calculadora funcion = new calculadora();
+        double num1;
+        double num2;
         do{
         System.out.println("Escribe los numeros que quieres utilizar(en orden)");
-        System.out.println("Escribe num1");
-        int num1 = sc.nextInt();
-        System.out.println("Escribe num2");
-        int num2 = sc.nextInt();
+        try{
+            System.out.println("Escribe num1");
+            num1 = sc.nextInt();
+            System.out.println("Escribe num2");
+            num2 = sc.nextInt();
+        }catch (java.util.InputMismatchException ex){
+            System.out.println("No escribiste un numero");
+            return;
+        }finally{
+
+        }
+        
         System.out.println("Elige una opcion" + "\n1-Sumar"
         + "\n2-Resta" + "\n3-Multiplicacion" + "\n4-Division");
         opciones = sc.nextInt();
 
         switch (opciones) {
             case 1:
-                int s = calculadora.suma(num1, num2);
+                double s = calculadora.suma(num1, num2);
                 break;
             case 2:
-                int r = calculadora.resta(num1, num2);
+                double r = calculadora.resta(num1, num2);
                 break;
             case 3:
-                int m = calculadora.multiplicar(num1, num2);
+                double m = calculadora.multiplicar(num1, num2);
                 break;
             case 4:
                 double d = calculadora.dividir(num1, num2);
